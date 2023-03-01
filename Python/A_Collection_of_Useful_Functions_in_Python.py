56a581f327b10227a466014dd2e9111ce695fd49
JB243 <55747737+JB243@users.noreply.github.com>
2023-03-01 16:48:52 +0900

    Update A_Collection_of_Useful_Functions_in_Python.py

--- Python/A_Collection_of_Useful_Functions_in_Python.py
+++ Python/A_Collection_of_Useful_Functions_in_Python.py
@@ -194,7 +194,41 @@ def two_image_correlation(img1_dir, img2_dir):
     plt.xlabel('brightness of img1')
     plt.ylabel('brightness of img2')
     # sns.regplot(l_b_CD31, l_b_Lipo, alpha = 0.05)
+
+def two_image_correlation_RG_3D(my_dir):
+
+    images = glob.glob(my_dir + "*.jpg")
     
+    l_img1 = []
+    l_img2 = []
+
+    for k in range(len(images)):
+        print(k)
+        img1 = cv2.imread(images[k]) 
+        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+        for i in range(img1.shape[0]):
+            for j in range(img1.shape[1]):
+                if (img1[i:i+1, j:j+1, 0:1] + img1[i:i+1, j:j+1, 1:2] + img1[i:i+1, j:j+1, 2:3]) != 0:
+                    l_img1.append((
+                        img1[i:i+1, j:j+1, 0:1] 
+                    ).item())
+
+                    l_img2.append((
+                        img1[i:i+1, j:j+1, 1:2] 
+                    ).item())
+
+    print("brightness of img1")
+    print(np.mean(l_img1))
+    print("brightness of img2")
+    print(np.mean(l_img2))
+
+    print("img1-img2 correlation")
+    print(scipy.stats.pearsonr(l_img1, l_img2) )
+
+    plt.scatter(l_img1, l_img2, alpha = 0.01)
+    plt.xlabel('brightness of img1')
+    plt.ylabel('brightness of img2')
+	
 def RGBtoGray(img):
     import numpy as np
 
